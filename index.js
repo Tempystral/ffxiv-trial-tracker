372ecd4ced7e070d26f5b1a513b9556bd845522c
Tempystral <elijah.doern@gmail.com>
2022-05-20 18:02:49 -0400

    ok forget about fastify for now

--- index.js
+++ index.js
@@ -1,57 +1,46 @@
 // index.js
-import Fastify from "fastify";
-import pointOfView from "point-of-view";
-import pugjs from "pug";
-
+import express from "express";
 import fs from "fs";
 import path from "path";
-import fastifyStatic from "@fastify/static";
 
 /**
  * App Variables
  */
 
-const app = Fastify({logger:true})
+const app = express();
 const port = process.env.PORT || 8000;
 const data = JSON.parse(fs.readFileSync('./data/data.json'))
+
 
 /**
  *  App Configuration
  */
 
-
-app.register(pointOfView, { engine: { pug: pugjs }, root: path.join(path.dirname("."), "views")});
-app.register(fastifyStatic, {root: path.join(path.dirname("."), "public"),
-                             prefix: "/public/",
-                             wildcard: false,
-                             serve: false
-                            });
-
-//app.set("view engine", "pug");
-//app.use("/public", express.static(path.dirname(".") + "/public"))
+app.set("views", path.join(path.dirname("."), "views"));
+app.set("view engine", "pug");
+app.use("/public", express.static(path.dirname(".") + "/public"))
 
 /**
  * Routes Definitions
  */
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   //res.status(200).send("THIS IS A TEST");
-  return res.view("index.pug", { title: "Home",
-                                data: data,
-                                scalefactor: {
-                                  "fullhd": 24,
-                                  "widescreen": 32,
-                                  "desktop": 36,
-                                  "tablet": 48
-                                }
-                              }
-          );
+  res.render("index", { title: "Home",
+                        data: data,
+                        scalefactor: {
+                          "fullhd": 24,
+                          "widescreen": 32,
+                          "desktop": 36,
+                          "tablet": 48
+                        }
+                      });
 });
 
 /**
  * Server Activation
  */
 
-await app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
