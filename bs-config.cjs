927f0be52caccbc61ce25a9871f2e3b64971a380
Tempystral <elijah.doern@gmail.com>
2022-05-21 19:14:52 -0400

    Small update

--- bs-config.cjs
+++ bs-config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     proxy: "localhost:8000",
-    files: ["**/*.css", "**/*.pug", "**/*.js"],
+    files: ["**/*.css", "**/*.pug", "**/*.js", "data/*.json"],
     ignore: ["node_modules"],
     reloadDelay: 10,
     ui: false,
