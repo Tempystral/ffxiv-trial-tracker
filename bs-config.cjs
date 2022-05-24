3028274cd85c60720ead99b2594d39f0eb3375ef
Tempystral <elijah.doern@gmail.com>
2022-05-24 01:32:06 -0400

    Start over with vue

--- bs-config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    proxy: "localhost:8000",
-    files: ["**/*.css", "**/*.pug", "**/*.js", "data/*.json"],
-    ignore: ["node_modules"],
-    reloadDelay: 10,
-    ui: false,
-    notify: false,
-    port: 8888,
-  };
