adc1c3ee58542fd8b0c491e2e7fefa07f995740d
Tempystral <elijah.doern@gmail.com>
2022-05-24 17:14:45 -0400

    Project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuePugPlugin from "vue-pug-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,17 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true
   },
-  plugins: [vue()]
+  plugins: [
+    vue({
+      template: {
+        preprocessOptions: {
+          plugins: [
+            {
+              preCodeGen: vuePugPlugin
+            }
+          ]
+        }
+      }
+    })
+  ]
 });
