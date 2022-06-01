ee2dd2c2a1c726163c6e6eccb756742abf6525c5
Tempystral <elijah.doern@gmail.com>
2022-06-01 02:58:37 -0400

    Start modal work

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,24 +1,63 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useStore } from '../ts/store';
+
 
 defineProps<{
 	show: boolean
 }>();
 
+const resetText = ref("Reset");
+const resetConfirm = ref(false);
+const store = useStore();
+
 // These are the signals the modal can output for other components to listen to
 const emit = defineEmits([
-	"close",
+	"close"
 ])
 
 // Output a close emit to whatever component is listening
 function closeModal() {
+	resetConfirm.value = false;
+	resetText.value = "Reset";
 	emit("close")
 }
+
+function reset() {
+	if (!resetConfirm.value) {
+		resetText.value = "Really reset data?";
+		resetConfirm.value = true;
+	}
+	else if (resetConfirm.value) {
+		resetText.value = "Data reset";
+		store.$state = { duties: [] };
+		console.log("Data reset");
+	}
+}
+
+
 </script>
 <!-- The onClick calls closeModal() which emits signal "close" -->
 <template lang="pug">
 .modal(:class="{ 'is-active': show }")
 	.modal-background( @click="closeModal" )
-	.modal-content
-		.box
-			p This is some text
+	.modal-card
+		header.modal-card-head
+			p.modal-card-title Settings 
+		section.modal-card-body 
+			.box
+				p Theme 
+			.box
+				button.button.is-danger(@click="reset") #{resetText}
+			.box
+				p Credits 
+		footer.modal-card-foot.is-block
+			.level
+				.level-left
+					.level-item
+						p.subtitle.is-5 © Tempystral #{new Date().getFullYear()}
+				.level-right
+					.level-item 
+						a.button Close 
+
 </template>
