a071fba8dddea8e5129a76876f53ca04ea091380
Tempystral <elijah.doern@gmail.com>
2022-06-02 02:29:30 -0400

    Remove double SASS import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Modal from '/components/Modal.vue';
-import sassStyles from '/assets/sass/style.scss';
 import localforage from 'localforage';
 import { useStore } from './ts/store';
 import { getLocalForageSafe, isUserData } from './ts/util';
