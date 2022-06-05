a1ca9a4dce877f059bf6f57a87e080346d96a05b
Tempystral <elijah.doern@gmail.com>
2022-06-05 01:10:43 -0400

    Update raid layout, raid reward data structures

--- src/components/Duties/Raid.vue
+++ src/components/Duties/Raid.vue
@@ -1,8 +1,9 @@
 <script setup lang='ts'>
 import localforage from "localforage";
-import { ref } from "vue";
+import { ref, toRef } from "vue";
 import { useStore } from "../../ts/store";
-import { Objective, type Duty } from "../../types";
+import { Objective, type RewardType, type Duty } from "../../types";
+import { getFullRewardName, getRewardImg } from "../../ts/util";
 
 const props = defineProps<{
 	dutyType: string,
@@ -11,19 +12,33 @@ const props = defineProps<{
 
 const store = useStore();
 const elemToShine = ref("");
+// Set up reward slices
+const rewards = toRef(props, "duty").value.rewards || null
+let normalRewards: Array<RewardType> = [];
+let savageRewards: Array<RewardType> = [];
+if (rewards != null) {
+	normalRewards = rewards.filter(r => { return r.objective == "normal" })
+	savageRewards = rewards.filter(r => { return r.objective == "savage" })
+}
 
 function markDuty(obj: string) {
 	store.markDone(props.duty, obj);
 	console.log(`Marked duty ${props.duty.id} as done`);
 	shimmer(obj);
 }
-
+function markReward(reward: RewardType) {
+	store.markCollected(props.duty, reward)
+	console.log(`Marked duty ${props.duty.name}, ${reward.item} as collected`)
+	shimmer(reward.item)
+}
 function shimmer(obj: string) {
 	elemToShine.value = obj;
 	setTimeout(() => {
 		elemToShine.value = "";
 	}, 700);
 }
+const isShining = (str: string) => { return elemToShine.value === str }
+function getRaidSet(diff: string) { return props.duty.rewards.find(r => { r.name == diff }) }
 
 </script>
 
@@ -31,20 +46,22 @@ function shimmer(obj: string) {
 .duty.raid.tile.is-child.box(:id="dutyType + '-' + duty.id")
 	.columns.is-gapless.is-multiline()
 		.column.is-2.reward-container
-			for reward in duty.rewards 
-				.image.is-48x48
-					img(:src='`/assets/img/icon/reward_${reward.name}.png`')
-					.fill-element(v-if='store.isObjectiveComplete(duty, reward.name)')
+			for reward in normalRewards
+				.reward.image.is-48x48(@click="markReward(reward)" :data-tooltip="getFullRewardName(reward)")
+					img(:src='getRewardImg(reward.name)')
+					.fill-element(v-if='store.isRewardCollected(duty, reward)')
 						img.X(src="/assets/img/X.png")
+					div.shine-target( :class='{ "shine shine-anim": isShining(reward.item) }')
 
 		.column.is-2.objective-container
 			for obj in duty.objectives
-				.level.mb-0(@click='markDuty(obj.name)' :class='{ "shine shine-anim": elemToShine === obj.name }')
+				.level.mb-0(@click='markDuty(obj.name)')
 					.level-left
 						.level-item.image.is-48x48
 							img(:src='`/assets/img/icon/duty_raid_${obj.name}.png`')
 							.fill-element(v-if='store.isObjectiveComplete(duty, obj.name)')
 								img.X(src="/assets/img/X.png")
+							div.shine-target( :class='{ "shine shine-anim": isShining(obj.name) }')
 						.level-item
 							h5.mb-0 #{obj.name.toLocaleUpperCase()}
 
@@ -63,4 +80,5 @@ function shimmer(obj: string) {
 				img(:src='duty.image')
 			.raid-content-title-container
 				.duty-title.title.is-4 #{duty.name}
+	//- div.shine-target(:class='{ "shine shine-anim": isShining(duty.name) }')
 </template>
